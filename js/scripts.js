7bf1d63971c9f18dfae9570039f0c5d45c1c4a15
Grace Adamiec and William Wittenbrock <william@>
2018-08-15 11:34:16 -0700

    Displays false if a user inputs a year that is divisible by 100.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@
 var leapYear = function(year) {
   if (year % 4 === 0 || year % 400 === 0)
     return true;
+  } else if (year % 100 === 0) {
+    return false
+  }
   else {
     return false;
   }
